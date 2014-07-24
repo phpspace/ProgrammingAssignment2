c6d1c26bfd4d8218e8af5e50dc6a8d785ca34d3f
phpspace <phpspace@users.noreply.github.com>
2014-07-24 10:44:32 -0300

    Programming Assignment - Cached inverse implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to create "special matrix" and return its inverse. This "special matrix" is able to cache its inverse
 
-## Write a short comment describing this function
+## Create the special "matrix" from a matrix "x" (this function assumes that "x" is invertible)
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  
+  set <- function(y) { # sets the value of the matrix
+    x <<- y
+    inv <<- NULL # If the matrix is changed, cached inverse should be updated afterwards
+  }
+  get <- function() x # get the value of the matrix
+  
+  setinverse <- function(inverse) inv <<- inverse # stores the inverse of "x" in the cache
+  getinverse <- function() inv # gets the value of the inverse of "x" in the cache
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Return a matrix that is the inverse of "x" 
+## Note that the input "x" is a "special matrix" created by makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse() # Get the current cached value of the inverse
+  
+  if(!is.null(inv)) { # If the current cached value is not null, then return the inverse
+    message("getting cached inverse")
+    return(inv)
+  }
+  
+  # Otherwise, inverse has to calculated and returned
+  message("calculating inverse (and caching)")
+  data <- x$get()         # Get matrix data
+  inv <- solve(data, ...) # Calculate inverse
+  x$setinverse(inv)       # Cache inverse of "x"
+  inv
 }
